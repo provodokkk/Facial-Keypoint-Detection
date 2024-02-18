ea553a141221bf37157aa6bcf851c5df821a9843
provodokkk <danylo.provodov@gmail.com>
2024-02-18 11:26:03 +0100

    Initial commit of the config.py file

---
+++ src/config.py
@@ -0,0 +1,12 @@
+INPUT_PATH = './input'
+
+EPOCHS = 100
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+IMG_RESIZE = 96  # in pixels
+
+# train-test split
+TEST_SPLIT = 0.2
+
+# show dataset keypoint plot
+SHOW_DATASET_PLOT = True
